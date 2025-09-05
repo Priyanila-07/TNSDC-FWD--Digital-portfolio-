02e2db26ff57559925acdbfd9d1755545cabb4eb
Priyanila-07 <priyaparvathi410@gmail.com>
2025-09-05 10:29:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Simple form alert
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+
+  e.preventDefault();
+
+  alert("Thank you! Your message has been sent successfully 🚀");
+
+  this.reset();
+
+});
